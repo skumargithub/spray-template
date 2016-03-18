fe691edc7d984b233c0d23f6983d2f00a9926f95
Senthil Kumar <skumaremail@yahoo.com>
2016-03-18 01:15:26 -0600

    Added akka ask example

--- src/main/scala/com/example/MyService.scala
+++ src/main/scala/com/example/MyService.scala
@@ -1,9 +1,15 @@
 package com.example
 
 import akka.actor.{Actor, Props}
+import akka.pattern.ask
+import akka.util._
 import spray.routing._
 import spray.http._
 import MediaTypes._
+
+import scala.concurrent.duration._
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.util.{Success, Failure}
 
 // we don't implement our route structure directly in the service actor because
 // we want to be able to test it independently, without having to spin up an actor
@@ -16,9 +22,21 @@ class MyServiceActor extends Actor with MyService {
   val childRequestHandlerActor = actorRefFactory.actorOf(Props[ChildRequestHandlerActor], "child-actor")
 
   val childRoutes =
-    path("fruit") {
+    path("fruit") {          // Passes the request context as part of the akka "tell"
       get { ctx =>
         childRequestHandlerActor ! GetFruit(ctx)
+      }
+    } ~
+    path("animal") {          // Uses akka ask
+      get { ctx =>
+        implicit val timeout = Timeout(5 seconds)
+        val futureReq = childRequestHandlerActor ? GetAnimal
+        futureReq.onComplete(x => {
+          x match {
+            case Success(s) => ctx.complete(s"$s")
+            case Failure(ex) => ctx.complete(StatusCodes.InternalServerError, s"$ex")
+          }
+        })
       }
     }
 
@@ -30,10 +48,13 @@ class MyServiceActor extends Actor with MyService {
 }
 
 case class GetFruit(ctx: RequestContext)
+case object GetAnimal
 
 class ChildRequestHandlerActor extends Actor {
 
   private val fruits = Array("apple", "banana", "pear", "strawberry", "orange", "peach")
+  private val animals = Array("donkey", "cat", "dog", "pig", "elephant", "zebra", "giraffe")
+
   private val rndm = scala.util.Random
 
   // this actor only runs our route, but you could add
@@ -41,6 +62,7 @@ class ChildRequestHandlerActor extends Actor {
   // or timeout handling
   def receive = {
     case GetFruit(ctx) => ctx.complete(s"Have this fruit ${fruits(rndm.nextInt(fruits.length))} from child")
+    case GetAnimal => sender ! s"Have this animal ${animals(rndm.nextInt(animals.length))} from child"
   }
 }
 
